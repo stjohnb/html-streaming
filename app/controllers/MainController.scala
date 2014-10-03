eb1dc87b9a77afd919dde0fcac1a59053108dd93
Brendan StJohn <bstjohn@gilt.com>
2014-10-03 19:21:50 +0100

    Adding more detail on enumerators

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -9,6 +9,7 @@ import play.api.templates.HtmlFormat
 
 import ui.HtmlStreamImplicits._
 import ui.{Pagelet, HtmlStream}
+import play.api.libs.iteratee.Enumerator
 
 object CartService {
   def cart = Promise.timeout("5 items in your cart", 3000)
@@ -30,13 +31,28 @@ object MainController extends Controller {
     } yield Ok(views.html.index(cart, waitlist))
   }
 
+  def helloEnumerator = Action {
+    Ok.chunked(Enumerator("Hello\n", "world\n", "enumerator\n"))
+  }
+
+  def repeat = Action {
+    Ok.chunked(Enumerator.repeatM(Promise.timeout("Hello\n", 200)))
+  }
+
+  def enumerators = Action {
+    val hello = Enumerator.repeatM(Promise.timeout("Hello\n", 200))
+    val goodbye = Enumerator.repeatM(Promise.timeout("goodbeye\n", 1000))
+
+    Ok.chunked(hello.interleave(goodbye))
+  }
+
   def andThen = Action {
 
     val cartF: Future[String] = CartService.cart
     val waitlistF: Future[String] = WaitlistService.waitlist
 
-    val cartStream = HtmlStream(cartF.map(s => views.html.partial(s)))
-    val waitlistStream = HtmlStream(waitlistF.map(s => views.html.partial(s)))
+    val cartStream = HtmlStream(cartF.map(s => views.html.cart(s)))
+    val waitlistStream = HtmlStream(waitlistF.map(s => views.html.waitlist(s)))
 
     val body = cartStream.andThen(waitlistStream)
 
@@ -48,8 +64,8 @@ object MainController extends Controller {
     val cartF: Future[String] = CartService.cart
     val waitlistF: Future[String] = WaitlistService.waitlist
 
-    val cartS = HtmlStream(cartF.map(s => views.html.partial(s)))
-    val waitlistS = HtmlStream(waitlistF.map(s => views.html.partial(s)))
+    val cartS = HtmlStream(cartF.map(s => views.html.cart(s)))
+    val waitlistS = HtmlStream(waitlistF.map(s => views.html.waitlist(s)))
 
     val body = HtmlStream.interleave(cartS, waitlistS)
 
@@ -61,8 +77,8 @@ object MainController extends Controller {
     val cartF: Future[String] = CartService.cart
     val waitlistF: Future[String] = WaitlistService.waitlist
 
-    val cartHtml: Future[HtmlFormat.Appendable] = cartF.map(s => views.html.partial(s))
-    val waitlistHtml: Future[HtmlFormat.Appendable] = waitlistF.map(s => views.html.partial(s))
+    val cartHtml: Future[HtmlFormat.Appendable] = cartF.map(s => views.html.cart(s))
+    val waitlistHtml: Future[HtmlFormat.Appendable] = waitlistF.map(s => views.html.waitlist(s))
 
     val cartS: HtmlStream = Pagelet.renderStream(cartHtml, "cart")
     val waitlistS: HtmlStream = Pagelet.renderStream(waitlistHtml, "waitlist")
